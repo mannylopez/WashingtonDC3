8a533a2748b147e4036f7ad3b5eec9696468bed6
mannylopez <manny@mapbox.com>
2015-11-29 21:25:51 -0500

    raster version of DC trip

---
+++ assets/js/garmin.js
@@ -0,0 +1,33 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoibWFubnkiLCJhIjoiMzBCNHFQUSJ9.VQQ9ZSW7viFT1yLhLiWLSA';
+
+var map = L.mapbox.map('map', 'mapbox.streets')
+  .setView([38.918209, -77.036801], 12);
+
+var one = omnivore.gpx('assets/data/1.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/2.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/3.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/4.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/5.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/6.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/7.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/8.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/9.gpx').addTo(map);
+// var one = omnivore.gpx('assets/data/10.gpx').addTo(map);
+// var one = omnivore.gpx('assets/data/11.gpx').addTo(map);
+// var one = omnivore.gpx('assets/data/12.gpx').addTo(map);
+// var one = omnivore.gpx('assets/data/13.gpx').addTo(map);
+// var one = omnivore.gpx('assets/data/14.gpx').addTo(map);
+// var one = omnivore.gpx('assets/data/15.gpx').addTo(map);
+// var one = omnivore.gpx('assets/data/16.gpx').addTo(map);
+// var one = omnivore.gpx('assets/data/17.gpx').addTo(map);
+// var one = omnivore.gpx('assets/data/18.gpx').addTo(map);
+// var one = omnivore.gpx('assets/data/19.gpx').addTo(map);
+// var one = omnivore.gpx('assets/data/20.gpx').addTo(map);
+// var one = omnivore.gpx('assets/data/21.gpx').addTo(map);
+// var one = omnivore.gpx('assets/data/22.gpx').addTo(map);
+// var one = omnivore.gpx('assets/data/23.gpx').addTo(map);
+// var one = omnivore.gpx('assets/data/24.gpx').addTo(map);
+// var one = omnivore.gpx('assets/data/25.gpx').addTo(map);
+// var one = omnivore.gpx('assets/data/26.gpx').addTo(map);
+// var one = omnivore.gpx('assets/data/27.gpx').addTo(map);
+// var one = omnivore.gpx('assets/data/28.gpx').addTo(map);

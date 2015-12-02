063cf5d6c9210caebff2b2cfcf0ee93c1048aa18
mannylopez <manny@mapbox.com>
2015-12-02 00:24:37 -0500

    Add Tuesday and Wednesday

--- assets/js/garmin.js
+++ assets/js/garmin.js
@@ -1,6 +1,6 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoibWFubnkiLCJhIjoiMzBCNHFQUSJ9.VQQ9ZSW7viFT1yLhLiWLSA';
 
-var map = L.mapbox.map('map', 'mapbox.streets')
+var map = L.mapbox.map('map', 'manny.oaa7pkam')
   .setView([38.918209, -77.036801], 12);
 
 var one = omnivore.gpx('assets/data/1.gpx').addTo(map);
@@ -12,14 +12,14 @@ var one = omnivore.gpx('assets/data/6.gpx').addTo(map);
 var one = omnivore.gpx('assets/data/7.gpx').addTo(map);
 var one = omnivore.gpx('assets/data/8.gpx').addTo(map);
 var one = omnivore.gpx('assets/data/9.gpx').addTo(map);
-// var one = omnivore.gpx('assets/data/10.gpx').addTo(map);
-// var one = omnivore.gpx('assets/data/11.gpx').addTo(map);
-// var one = omnivore.gpx('assets/data/12.gpx').addTo(map);
-// var one = omnivore.gpx('assets/data/13.gpx').addTo(map);
-// var one = omnivore.gpx('assets/data/14.gpx').addTo(map);
-// var one = omnivore.gpx('assets/data/15.gpx').addTo(map);
-// var one = omnivore.gpx('assets/data/16.gpx').addTo(map);
-// var one = omnivore.gpx('assets/data/17.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/10.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/11.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/12.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/13.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/14.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/15.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/16.gpx').addTo(map);
+var one = omnivore.gpx('assets/data/17.gpx').addTo(map);
 // var one = omnivore.gpx('assets/data/18.gpx').addTo(map);
 // var one = omnivore.gpx('assets/data/19.gpx').addTo(map);
 // var one = omnivore.gpx('assets/data/20.gpx').addTo(map);
